66990ef4021c7ede279c5e3f0eb4874b99a2704b
Mark Silverberg <mark@marksilver.net>
2022-01-30 13:24:19 -0600

    Handle view submission: parse inputs, validate inputs, send DM confirmation

--- app.js
+++ app.js
@@ -29,6 +29,44 @@ app.shortcut('globalShortcut_featureRequest', async ({ shortcut, ack, client, lo
     view: fileABugModalPayload()
   })
 })
+
+// Listen for form/modal submission
+app.view('fileABugModal', async ({ ack, body, view, client, logger, respond }) => {
+  // Extract user-submitted values from view submission object
+  const {
+    block_priority: { input_priority: { selected_option: { value: priority } } },
+    block_title: { input_title: { value: title } },
+    block_description: { input_description: { value: description } }
+  } = view.state.values
+  logger.debug('User submitted values: ', { priority, title, description })
+
+  // Validate inputs
+  const errors = {}
+  if (description.length < 10) {
+    errors.block_description = 'Description must be at least 10 characters'
+  }
+
+  // If there are errors, respond to Slack with errors; otherwise, respond with a confirmation
+  if (Object.keys(errors).length > 0) {
+    console.warn('errors: ', errors)
+    await ack({
+      response_action: 'errors',
+      errors
+    })
+  } else {
+    // If there are no errors, close the modal and DM the user a confirmation
+    await ack()
+
+    const dmToSubmitter = await client.chat.postMessage({
+      channel: body.user.id,
+      text: 'Thanks for your bug report! We will triage it ASAP.' + '\n' + '\n' +
+            '*Priority:* ' + priority + '\n' +
+            '*Title:* ' + title + '\n' +
+            '*Description:* \n> ' + description.split('\n').join('\n> ')
+    })
+  }
+
+  // TODO: Save form response to Airtable
 })
 
 // Listens to any incoming messages
